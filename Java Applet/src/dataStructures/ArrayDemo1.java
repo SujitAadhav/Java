5fc0688e10ea961d6a8aa45a8088d3e42476ac49
“Sujit <sujitadhav17@gmail.com>
2022-06-23 10:00:59 +0530

    Data Structures

---
+++ Java Applet/src/dataStructures/ArrayDemo1.java
@@ -0,0 +1,17 @@
+package dataStructures;
+
+import java.awt.Button;
+
+public class ArrayDemo1 {
+	int a[] = {2,3,4,5};
+	Button b[] = new Button[5];
+	
+	public void print() {
+		for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
+}
